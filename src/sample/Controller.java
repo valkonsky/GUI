d05e526bc7a645d1efc1de0f9bf2a041241d7a15
savilin <valkonskyspb@icloud.com>
2022-02-25 20:30:22 +0300

    GUI start add window chat

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
+
+    public void btn1click(ActionEvent actionEvent) {
+
+        System.out.println("Hello world!");
+    }
+
+    public void sendMessage(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText()+ "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
